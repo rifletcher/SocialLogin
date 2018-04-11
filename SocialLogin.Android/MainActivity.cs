214bb0a1b4956792550d93a1450f01fac1466ecc
Robert Fletcher <rifletcher@hotmail.com>
2018-04-11 16:11:51 +0300

    initial commit

---
+++ SocialLogin.Android/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Acr.UserDialogs;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using SimpleAuth;
+
+[assembly: MetaData("com.facebook.sdk.ApplicationId", Value = "@string/facebook_app_id")]
+namespace SocialLogin.Droid
+{
+    [Activity(Label = "SocialLogin", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            SimpleAuth.Providers.Facebook.Init(this.Application, false);
+
+            UserDialogs.Init(this);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            Native.OnActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
+
---
+++ SocialLogin/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SimpleAuth;
+
+namespace SocialLogin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var clientId = "627982777551085";
+
+
+            // Odd, you need to add the client secret as the third parameter here, if using UWP
+            var fb = new SimpleAuth.Providers.FacebookApi("facebook", clientId, "");
+
+            var account = await AuthAsync(fb);
+
+            Acr.UserDialogs.UserDialogs.Instance.Alert(
+                "Account Returned",
+                account.ToJson(), "Ok");
+        }
+
+        async Task<Account> AuthAsync(OAuthApi api)
+        {
+            Account result = null;
+            try
+            {
+                result = await api.Authenticate();
+            }
+            catch (TaskCanceledException ex1)
+            {
+                Acr.UserDialogs.UserDialogs.Instance.Alert(
+                    "Exception",
+                    ex1.ToString(), "Ok");
+
+            }
+            catch (Exception ex)
+            {
+                Acr.UserDialogs.UserDialogs.Instance.Alert(
+                    "Exception",
+                    ex.ToString(), "Ok");
+                Console.WriteLine(ex);
+            }
+
+            if (result != null)
+                Console.WriteLine(await result.ToJsonAsync());
+
+            return result;
+        }
+
+    }
+}
